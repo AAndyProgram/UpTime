8c8c40176f4757bf51d3603e9c85e6e279819048
Andy <88590076+AAndyProgram@users.noreply.github.com>
2021-09-21 02:34:07 +0300

    Add project files.

---
+++ README.md
@@ -0,0 +1,7 @@
+# UpTime
+
+Simple tool for getting information about computer uptime
+
+Enjoying the tool? Considering adding to my coffee fund :)
+
+[![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/andyprogram)
---
+++ UpTime/MainMOD.vb
@@ -0,0 +1,11 @@
+﻿Public Module MainMOD
+    Public Sub Main()
+        Using p As New PerformanceCounter("System", "System Up Time")
+            p.NextValue()
+            Dim t As TimeSpan = TimeSpan.FromSeconds(p.NextValue())
+            MsgBox($"UpTime: {t.Days} days, {t.Hours} hours {t.Minutes} minutes!",
+                   MsgBoxStyle.Information + MsgBoxStyle.OkOnly + MsgBoxStyle.DefaultButton1,
+                   "UpTime...")
+        End Using
+    End Sub
+End Module
